8a98621f2c39af588cb779f6bb379eeadcb8925b
Daniel <danielcazacu205@gmail.com>
2026-07-06 14:14:07 +0300

    Format top module

--- RAM_Testbench/top_module.sv
+++ RAM_Testbench/top_module.sv
@@ -4,25 +4,27 @@
 
 module top_module;
 
-    bit clk;
-    initial clk = 0;
-    always #5 clk = ~clk;
+  bit clk;
+  initial
+    clk = 0;
+  always #5 clk = ~clk;
 
-    ram_inf inf(clk);
+  ram_inf inf(clk);
 
-    ram_design DUT(.clk(inf.clk),
-                   .rst(inf.rst),
-                   .w_addr(inf.w_addr),
-                   .rd_addr(inf.rd_addr),
-                   .data_in(inf.data_in),
-                   .data_out(inf.data_out));
+  ram_design DUT(.clk(inf.clk),
+                 .rst(inf.rst),
+                 .w_addr(inf.w_addr),
+                 .rd_addr(inf.rd_addr),
+                 .data_in(inf.data_in),
+                 .data_out(inf.data_out));
 
-    RAM_environment env;
+  RAM_environment env;
 
-    initial begin
-        env = new(inf, 40, 40);
-        env.run();
-        #1000;
-        $finish;
-    end
+  initial
+  begin
+    env = new(inf, 40, 40);
+    env.run();
+    #1000;
+    $finish;
+  end
 endmodule
